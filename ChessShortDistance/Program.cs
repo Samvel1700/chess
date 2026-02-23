718e4f4222d3d7d06bfdbee55db88c55b0c61928
mikayel5 <mikayelnahapetyan@gmail.com>
2026-02-23 18:57:30 +0400

    create_Bishop_move

--- ChessShortDistance/Program.cs
+++ ChessShortDistance/Program.cs
@@ -2,9 +2,6 @@
 using ChessShortDistance.Enums;
 using ChessShortDistance.Models;
 using ChessShortDistance.PiecesOneMoveSteps.ChessShortDistance;
-using System;
-using System.IO.Pipelines;
-using System.Xml.Linq;
 
 class Program
 {
@@ -16,7 +13,9 @@ class Program
         ConvertPorgram.Run(out start, out end);
         Console.Write("Write Your Pices Name: ");
         string PicesName = Console.ReadLine().ToUpper();
-        if (Enum.TryParse(PicesName, true, out Pices piece))
+
+
+        if (Enum.TryParse(PicesName, true, out Pices piece))//
         {
             switch (piece)
             {
@@ -47,12 +46,26 @@ class Program
                         Console.WriteLine($"{Pices.Knight} CAN NOT reach in ONE move!");
                     }
                     break;
-
+                case Pices.Bishop:
+                    if (KnightCanMoveInOneStep.Run(start, end))
+                    {
+                        Console.WriteLine($"{Pices.Knight} can reach in ONE move!");
+                        Console.WriteLine($"({start.Row}, {start.Col})");
+                        Console.WriteLine($"({end.Row}, {end.Col})");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Pices.Knight} CAN NOT reach in ONE move!");
+                    }
+                    break;
                 default:
                     Console.WriteLine("This piece is not implemented yet.");
                     break;
             }
         }
+        Console.WriteLine("Wrong Pices Name.");
+
         //KnightPath.RunProgram();
 
 
